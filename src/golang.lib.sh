0a811dec8644df2f7d65efbbd060805857408813
Thomas Tych <thomas.tych@gmail.com>
2020-05-15 17:43:38 +0200

    [golang] update base lib for golang

--- src/golang.lib.sh
+++ src/golang.lib.sh
@@ -4,7 +4,12 @@
 for home in "${HOME_ALT}" "${HOME}"; do
     if [ -d "$home" ]; then
         GOROOT="$home/local/$(uname -s)/$(uname -m)/go_root"
-        GOPATH="$GOROOT/go_root/packages"
+        GOPATH="$GOROOT/packages"
+        break
     fi
 done
 export GOROOT GOPATH
+
+if [ ! -d "$GOROOT" ]; then
+    mkdir -p "$GOROOT"
+fi
